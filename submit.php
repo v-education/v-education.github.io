d98205c5d871072c0ed3ac052a0f699e38a9ea7a
v-education <129520752+v-education@users.noreply.github.com>
2023-04-02 12:35:22 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,22 +1,22 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $wechat = $_POST['wechat'];
-  $phone = $_POST['phone'];
-  $course = $_POST['course'];
-
-  // Save the form data to a file or database
-  // ...
-
-  // Send an email notification to the course administrator
-  $to = 'umarovbek@outlook.com';
-  $subject = 'New course registration';
-  $message = "Name: $name\nEmail: $email\nWeChat: $wechat\nPhone: $phone\nCourse: $course";
-  $headers = "From: $email\r\n" .
-             "Reply-To: $email\r\n" .
-             "X-Mailer: PHP/" . phpversion();
-
-  mail($to, $subject, $message, $headers);
-}
-?>
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$wechat = $_POST["wechat"];
+		$phone = $_POST["phone"];
+		$course_fee = $_POST["course_fee"];
+		$course_time = $_POST["course_time"];
+		
+		// Add your email address here
+		$to = "umarovbek@outlook.com";
+		$subject = "New Application Submitted";
+		$message = "Name: ".$name."\r\n"."Email: ".$email."\r\n"."WeChat ID: ".$wechat."\r\n"."Phone Number: ".$phone."\r\n"."Course Fee: ".$course_fee."\r\n"."Course Time: ".$course_time;
+		$headers = "From: ".$email."\r\n"."Reply-To: ".$email."\r\n"."X-Mailer: PHP/".phpversion();
+		
+		if(mail($to, $subject, $message, $headers)){
+			echo "<p>Application submitted successfully. We will contact you as soon as possible!</p>";
+		} else{
+			echo "<p>Oops! Something went wrong. Please try again later.</p>";
+		}
+	}
+	?>
